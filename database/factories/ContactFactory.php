ebb2ef28f31888dd42ffc8a462bb1592459a9733
平山 健 <yumidaimajin@gmail.com>
2026-07-12 16:34:10 +0900

    feat: シーディング実装と初期データ投入完了

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'gender' => $this->faker->numberBetween(1, 3),
+            'email' => $this->faker->safeEmail(),
+            'tel' => $this->faker->numerify('###########'), // 11桁の数字
+            'address' => $this->faker->address(),
+            'building' => $this->faker->secondaryAddress(),
+            'detail' => $this->faker->realText(120),
+        ];
+    }
+}
---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Contact;
+use App\Models\Category;
+use App\Models\Tag;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = Category::all();
+        $tags = Tag::all();
+
+        Contact::factory()->count(20)->make()->each(function ($contact) use ($categories, $tags) {
+            $contact->category_id = $categories->random()->id;
+            $contact->save();
+            $contact->tags()->attach($tags->random(rand(1, 3))->pluck('id'));
+        });
+    }
+}
